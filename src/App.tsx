e89f6919e31f494c7ec429b2ec8e73f5904f5ea0
kmin <kmin@student.42seoul.kr>
2021-06-08 14:08:50 +0900

    refactor: number line 마무리
    onchange event를 활용해서 더 짧으면서 동일한 기능을 하는 코드를 만들 수
    있었다.

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,15 @@ import './App.module.css';
 import Editor from "./components/editor/editor";
 
 function App() {
-    const [code, setCode] = useState('');
-    const [lines, setLines] = useState(1);
+    const [code, setCode] = useState('alert("This is alert.")\n' +
+      'console.log("This is log.")');
+    const [hasLine, setHasLine] = useState(true);
+    const [placeHolder, setPlaceHolder] = useState('Wait for code...');
   return (
       <div className={styles.container}>
           <div className={styles.editor}>
-              <Editor code={code} setCode={setCode} lines={lines} setLines={setLines}
+              <Editor code={code} setCode={setCode}
+                      hasLine={hasLine} placeHolder={placeHolder}
               />
           </div>
       </div>
--- src/components/editor/editor.tsx
+++ src/components/editor/editor.tsx
@@ -1,44 +1,27 @@
-import React from 'react';
-import SimpleEditor from "react-simple-code-editor";
-import {Grammar, highlight, languages} from 'prismjs';
+import React, {useState} from 'react';
 import styles from './editor.module.css';
-import 'prismjs/components/prism-javascript';
-import 'prismjs/themes/prism-dark.css';
-import 'prismjs/components/prism-css';
 import LineNumber from './lineNumber/lineNumber';
 
 interface EditorProps {
   code: string;
   setCode: (nextCode: string) => void;
-  lines: number;
-  setLines: (nextLine: number) => void;
+  hasLine: boolean;
+  placeHolder: string;
 }
 
-const Editor = ({code, setCode, lines, setLines}: EditorProps) => {
-  const spaceLength = 9;
-  const spaceTemplate = (totalLength: number, current: number) => {
-    const currentLen = String(current).length;
-    return ' '.repeat(totalLength - (currentLen) * 2);
-  }
-  const codeWithLineNumbers = (code: string, language: Grammar) =>
-    highlight(code, language, 'js')
-      .split("\n")
-      .map((line, i) => {
-        const space = spaceTemplate(spaceLength, i + 1);
-        // const trimmedLine = line.replaceAll('\t', '        ');
-        return (`<span className={styles.editorLineNumber}>${i + 1}</span><span>${space}</span>${line}`
-        )
-      })
-      .join("\n");
+const Editor = ({
+                  code = 'alert("This is alert.")\n' +
+                  'console.log("This is log.")', setCode, hasLine,
+  placeHolder
+                }: EditorProps) => {
+
+  const [lines, setLines] = useState(2);
 
   const onPaste = (event: React.ClipboardEvent<HTMLTextAreaElement>) => {
-    event.preventDefault();
     const pastedData = event.clipboardData;
     const textData = pastedData?.getData('Text');
     const {target} = event;
     if (target instanceof HTMLTextAreaElement && textData) {
-      const newLines = textData.match(/\n/g)?.length! as number | 1;
-      setLines(newLines + 1);
       if (target.selectionEnd - target.selectionStart > 0) {
         target.setRangeText('', target.selectionStart, target.selectionEnd, 'end');
       }
@@ -46,17 +29,11 @@ const Editor = ({code, setCode, lines, setLines}: EditorProps) => {
     }
   }
 
-  const onKeyDown = (event: React.KeyboardEvent<HTMLTextAreaElement>) => {
-    if (event.key === 'Enter') {
-      const newLines = lines + 1;
-      setLines(newLines);
-    } else if (event.key === 'Backspace') {
-      const {target} = event;
-      if (target instanceof HTMLTextAreaElement &&
-        target.value && target.value[target.selectionStart-1] === '\n') {
-        const newLines = lines - 1;
-        setLines(newLines);
-      }
+  const onChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
+    const {target} = event;
+    if (target instanceof HTMLTextAreaElement) {
+      const newLines = target.value.match(/\n/g)?.length! as number ?? 1;
+      setLines(newLines + 1);
     }
   }
 
@@ -64,12 +41,12 @@ const Editor = ({code, setCode, lines, setLines}: EditorProps) => {
     <div className={styles.container}>
       <div>
         {
-          Array.from({length: lines}, (undefined, i) => i)
+          hasLine && Array.from({length: lines}, (undefined, i) => i)
             .map((v, i) => <LineNumber line={i + 1}/>)
         }
       </div>
-      <textarea className={styles.editor} onPaste={onPaste} onKeyDown={onKeyDown}
-                placeholder={"Wait for code..."} rows={40}>{code}
+      <textarea className={styles.editor} onPaste={onPaste} onChange={onChange}
+                placeholder={placeHolder} rows={40} wrap={"off"}>{code}
       </textarea>
     </div>
     // <SimpleEditor
